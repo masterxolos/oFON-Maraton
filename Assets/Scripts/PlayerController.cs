6ad2cd4ef5c973717b123b299e78b2d3691430f1
masterxolos <ozgur-2000-6@hotmail.com>
2022-02-08 15:05:15 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int _currentPillowCount = 0;
+
+    [SerializeField] private int pillowHeight;
+
+    [SerializeField] private Transform firstPillowLocation;
+    [SerializeField] private GameObject[] pillows;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void PickupPillow()
+    {
+        
+    }
+
+}
